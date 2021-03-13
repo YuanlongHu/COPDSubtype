e32317d185e66eda6eebabc4bffd3a365125b9ea
YuanlongHu <huyuanlong1996@163.com>
2021-03-13 16:51:19 +0800

    debug

--- best_fit.R
+++ best_fit.R
@@ -10,15 +10,39 @@ rl <- run_all_consensus_partition_methods(
   partition_method = c("hclust","kmeans","pam")
   )
 
-best_k <- cola::suggest_best_k(rl)
 
-best_k[best_k$`1-PAC`>0.9,] # fulfill second rule
+Best_fit <- function(res){
+  best_k <- cola::suggest_best_k(res)
+  if(nrow(best_k[best_k$`1-PAC`>0.9,])==0){
+    best_k2 <- apply(best_k[,-c(1,5)],2, function(x){
+    ifelse(x == max(x),1,0)
+   }) %>%
+    as.data.frame()
+   best_k2$vote <- apply(best_k2,1,sum)
+   best_k2 <- best_k2[best_k2$vote %in% max(best_k2$vote),]
+   best_k <- best_k[rownames(best_k2),]
+   
+   message("Fulfill third rule:\n")
+   message(paste(">>> Best-fit:", rownames(best_k2)))
+   message(">>> Parameters: ")
+   message(paste0("  beat k: ", best_k[1,1], "\n",
+                  "  1-PAC: ", best_k[1,2], "\n",
+                  "  mean silhouette: ", best_k[1,3], "\n",
+                  "  concordance: ", best_k[1,4]))
+   message(">>> Vote:")
+   message(paste0("  1-PAC: ", best_k2[1,1], "\n",
+                  "  mean silhouette: ", best_k2[1,2], "\n",
+                  "  concordance: ", best_k2[1,3]))
+   }else{
+    best_k <- best_k[best_k$`1-PAC`>0.9,]
+    message("Fulfill second rule")
+    return(best_k)
+  }
 
-best_k <- apply(best_k[,-c(1,5)],2, function(x){  # Third rule
-  ifelse(x == max(x),1,0)
-}) %>%
-  as.data.frame()
+}
 
-best_k$vote <- apply(best_k,1,sum)
-best_k[best_k$vote %in% max(best_k$vote),] # fulfill third rule
+
+Best_fit(rl)
+
+
 
